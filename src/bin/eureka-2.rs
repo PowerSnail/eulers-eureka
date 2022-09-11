c6e1e66931139d6f8052df1420e654c9ecc7ee4d
PowerSnail <hj@powersnail.com>
2022-09-11 09:20:48 -0700

    problem 2

---
+++ src/bin/eureka-2.rs
@@ -0,0 +1,31 @@
+fn naive() -> u64 {
+    let mut sum: u64 = 0;
+    let mut x: u64 = 1;
+    let mut y: u64 = 2;
+    while y <= 4_000_000 {
+        if y % 2 == 0 {
+            sum += y;
+        }
+        let x_ = x + y;
+        x = y;
+        y = x_;
+    }
+    sum
+}
+
+fn count_3() -> u64 {
+    let mut buffer = [1, 1, 2];
+    let mut sum = 0;
+    while buffer[2] < 4_000_000 {
+        sum += buffer[2];
+        buffer[0] = buffer[1] + buffer[2];
+        buffer[1] = buffer[2] + buffer[0];
+        buffer[2] = buffer[0] + buffer[1];
+    }
+    sum
+}
+
+fn main() {
+    // println!("{}", naive());
+    println!("{}", count_3());
+}
