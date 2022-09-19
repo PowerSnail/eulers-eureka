0e08274476835c90e95f4337302372f73e1e56a3
PowerSnail <hj@powersnail.com>
2022-09-19 11:18:14 -0700

    p5 solved

--- src/bin/eureka-5.rs
+++ src/bin/eureka-5.rs
@@ -1,18 +1,24 @@
 use eulers_eureka::primes_under;
 
+const TARGET: u64 = 20;
+
 fn naive() -> u64 {
-    for n in 21.. {
-        if (2..=20).all(|d| n % d == 0) {
+    for n in (TARGET+1).. {
+        if (2..=TARGET).all(|d| n % d == 0) {
             return n;
         }
     }
     unreachable!();
 }
 
+fn larget_exponent(max: u64, base: u64) -> u64 {
+    base.pow((max as f32).log(base as f32).floor() as u32)
+}
+
 fn prime_factors() -> u64 {
-    println!("{:?}", primes_under(11));
-    primes_under(11)
+    primes_under(TARGET+1)
         .into_iter()
+        .map(|n| larget_exponent(TARGET, n))
         .reduce(|x, y| x * y)
         .unwrap()
 }
